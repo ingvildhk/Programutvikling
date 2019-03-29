c73998bd5e63c92081373450c7ecedee11d42e32
renkjet <renate.kje@gmail.com>
2019-03-29 17:12:37 +0100

    Update KontaktPerson.java

--- src/main/java/org/kulturhusfx/base/KontaktPerson.java
+++ src/main/java/org/kulturhusfx/base/KontaktPerson.java
@@ -9,57 +9,57 @@ public class KontaktPerson {
     private String andreOpplysninger;
 
     //nettside, firma og andreOpplysninger er ikke i konstruktøren da de er valgfrie opplysninger
-    public KontaktPerson(String navn, String telefonnummer, String epost){
+    public KontaktPerson(String navn, String telefonnummer, String epost) {
         this.navn = navn;
         this.telefonnummer = telefonnummer;
         this.epost = epost;
     }
 
-    public String getNavn(){
+    public String getNavn() {
         return navn;
     }
 
-    public String getTelefonnummer(){
+    public String getTelefonnummer() {
         return telefonnummer;
     }
 
-    public String getEpost(){
+    public String getEpost() {
         return epost;
     }
 
-    public String getNettside(){
+    public String getNettside() {
         return nettside;
     }
 
-    public String getFirma(){
+    public String getFirma() {
         return firma;
     }
 
-    public String getAndreOpplysninger(){
+    public String getAndreOpplysninger() {
         return andreOpplysninger;
     }
 
-    public void setNavn(String navn){
+    public void setNavn(String navn) {
         this.navn = navn;
     }
 
-    public void setTelefonnummer(String telefonnummer){
-        this.telefonnummer =  telefonnummer;
+    public void setTelefonnummer(String telefonnummer) {
+        this.telefonnummer = telefonnummer;
     }
 
-    public void setEpost(String epost){
+    public void setEpost(String epost) {
         this.epost = epost;
     }
 
-    public void setNettside(String nettside){
+    public void setNettside(String nettside) {
         this.nettside = nettside;
     }
 
-    public void setFirma(String firma){
+    public void setFirma(String firma) {
         this.firma = firma;
     }
 
-    public void setAndreOpplysninger(String andreOpplysninger){
+    public void setAndreOpplysninger(String andreOpplysninger) {
         this.andreOpplysninger = andreOpplysninger;
     }
 
