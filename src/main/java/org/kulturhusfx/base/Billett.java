8be750d6e2794863dbc3ccb6a4e7970d9b917705
renkjet <renate.kje@gmail.com>
2019-03-29 14:55:11 +0100

    Create Billett.java

---
+++ src/main/java/org/kulturhusfx/base/Billett.java
@@ -0,0 +1,24 @@
+package org.kulturhusfx.base;
+
+public class Billett {
+
+        private int telefonnrKjoper;
+        private int plassnr;
+
+        public Billett(int telefonnrKjoper){
+            this.telefonnrKjoper = telefonnrKjoper;
+        }
+
+        public void setTelefonnrKjoper(int telefonnrKjoper){
+            this.telefonnrKjoper = telefonnrKjoper;
+        }
+        public int getTelefonnrKjoper() {
+            return telefonnrKjoper;
+        }
+
+        public int genererPlass(){
+
+
+        }
+    }
+
