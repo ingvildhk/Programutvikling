b65fe957af8c06f9d9d24c557bdc3dd1dc6f74f8
renkjet <renate.kje@gmail.com>
2019-03-29 13:57:51 +0100

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module hellofx {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens org.kulturhusfx to javafx.fxml;
+    opens org.kulturhusfx.controllers to javafx.fxml;
+
     exports org.kulturhusfx;
+    exports org.kulturhusfx.controllers;
 }
