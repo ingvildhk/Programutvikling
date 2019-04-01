df5ff5ecc5aefb3d472c7fdbc33dfb9d6a1f8d64
nektarinen <42868795+nektarinen@users.noreply.github.com>
2019-04-01 15:58:32 +0200

    maincontroller updated

--- src/main/java/org/kulturhusfx/controllers/AdminMainPageController.java
+++ src/main/java/org/kulturhusfx/controllers/AdminMainPageController.java
@@ -1,18 +1,69 @@
 package org.kulturhusfx.controllers;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.kulturhusfx.base.Hall;
+import org.kulturhusfx.base.InvalidNumberOfSeatsException;
+
+import java.io.IOException;
 
 public class AdminMainPageController {
 
-    //@FXML
-    //private Label label;
-
     @FXML
-    private void handleAdminLoginButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        //label.setText("Hello World!");
+    TextField roomName, roomType, totalNumberofSeats, performers, eventTime, ticketPrice, eventName;
+    @FXML
+    TextField contactName, contactPhone, contactEmail, contactWebsite, contactFirm, contactOther, eventDate;
+    @FXML
+    TextArea eventProgram;
+    @FXML
+    ChoiceBox eventType, eventRoom;
+
+    public void roomRegistrationBtn(ActionEvent event) throws InvalidNumberOfSeatsException {
+        String room = roomName.getText();
+        String type = roomType.getText();
+        String seat = totalNumberofSeats.getText();
+        Hall newHall = new Hall(room, type, seat);
+        newHall.checkValidNumberOfSeats(seat);
+
+        System.out.println(newHall.getHallName() + newHall.getHallType() + newHall.getNumberOfSeats());
+    }
+    
+    public void backToMainPageBtn(ActionEvent event) throws  IOException{
+        Parent MainPageParent = null;
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            MainPageParent = fxmlLoader.load(getClass().getResource("MainPage.fxml").openStream());
+        } catch (IOException e) {
+            e.printStackTrace(); // FXML document should be available
+            return;
+        }
+
+        /*
+        Scene MainPageScene = new Scene(MainPageParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(MainPageScene);
+        window.show();*/
+
+    }
+
+    public void eventRegistrationBtn(ActionEvent event) throws IOException {
+        System.out.println("I work too!");
+    }
+
+    public void manageEventsBtn(ActionEvent event){
+
+    }
+
+    public void seeAllEventsBtn(ActionEvent event){
+
     }
 
     public void initialize() {
