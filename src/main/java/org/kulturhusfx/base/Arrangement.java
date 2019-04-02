21109ac1d36e04b1b4a8be3a9735962a6e06152f
renkjet <renate.kje@gmail.com>
2019-04-02 08:31:48 +0200

    Små endringer i checkValidDate-metoden

--- src/main/java/org/kulturhusfx/base/Arrangement.java
+++ src/main/java/org/kulturhusfx/base/Arrangement.java
@@ -1,5 +1,7 @@
 package org.kulturhusfx.base;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Arrangement {
 
@@ -38,6 +40,19 @@ public class Arrangement {
 
     public void endreArrangement(Arrangement o) {
     }
+
+    // Metode for å sjekke om Dato-input er i riktig format
+    private boolean checkValidDate(String date) throws InvalidDateException {
+        String regex = "^(1[0-2]|0[1-9])/(3[01]|[12][0-9]|0[1-9])/[0-9]{4}$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(date);
+        boolean validDate = m.matches();
+        if (validDate == false) throw new InvalidDateException
+                ("Husk å skrive inn dato i riktig format, feks: 12/12/2013. Arrangement ble ikke oppdatert ");
+        return true;
+    }
+
+
 
 
 }
