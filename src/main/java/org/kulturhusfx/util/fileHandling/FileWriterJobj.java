826027c991a84fe58ddc82714890737848810128
IngvildK <42868795+nektarinen@users.noreply.github.com>
2019-04-12 13:28:08 +0200

    filewriter

--- src/main/java/org/kulturhusfx/util/fileHandling/FileWriterJobj.java
+++ src/main/java/org/kulturhusfx/util/fileHandling/FileWriterJobj.java
@@ -1,6 +1,8 @@
 package org.kulturhusfx.util.fileHandling;
 
+import org.kulturhusfx.base.Event;
 import org.kulturhusfx.base.Hall;
+import org.kulturhusfx.base.Ticket;
 
 import java.io.FileOutputStream;
 import java.io.FileWriter;
@@ -26,11 +28,25 @@ public class FileWriterJobj extends FileWriter {
         }
     }
 
-    public static void saveEventToFile(){
+    public static void saveEventToFile(Event event, String filePath) throws IOException{
+        try (
+                FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        ) {
+            objectOutputStream.writeObject(event);
+            System.out.println("Skrevet til fil");
 
+        }
     }
 
-    public static void saveTicketToFile(){
+    public static void saveTicketToFile(Ticket ticket, String filePath) throws IOException{
+        try (
+                FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        ) {
+            objectOutputStream.writeObject(ticket);
+            System.out.println("Skrevet til fil");
 
+        }
     }
 }
